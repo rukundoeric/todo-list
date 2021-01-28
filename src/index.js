6d729b886cf6eb60294660992238c551667cd12c
Rukundo Eric <ericrukundo005@gmail.com>
2021-01-28 23:25:08 +0200

    modify project & todo-item

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello there!');
+const projectList = [
+  {
+    name: 'Project name',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, architecto.',
+    tasks: [
+      {
+        title: 'How to create USER',
+        dueDate: '23/12/2020',
+      },
+      {
+        title: 'How to create USER',
+        dueDate: '23/12/2020',
+      },
+      {
+        title: 'How to create USER',
+        dueDate: '23/12/2020',
+      },
+    ],
+  },
+];
